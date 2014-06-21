7bc148f013933ba1fd9b3f2061af1fb30c58c9bb
BenjaminMassey <benjamin.w.massey@gmail.com>
2014-06-21 15:59:26 -0700

    changed health calculation
    Health didn't work like I thought it did, so I changed it to how it is
    suppose to calculate. Now the program asks for all of your stats, as
    they are all needed to determine your health. The calculation is still
    slightly off though, still not sure why. Added some more info in the
    README

--- DS2Stats.py
+++ DS2Stats.py
@@ -9,25 +9,13 @@
 
 level = int(input("Please enter your soul level: "))
 
-if level < 21:
-    LevelHealth = (2 * level)
-
-if level > 21:
-    LevelHealth = 40 + (level - 20)
+LevelHealth = 0
 
 # Vigor
+# The calculations for health are very strange, so these
+# are purely estimates.
 
 vigor = int(input("Please enter your vigor: "))
-
-lowvigor = (30 * vigor) + LevelHealth
-
-if vigor < 21:
-    print("Your character should have around " + str(lowvigor) + " HP.")
-
-midvigor = (30 * (vigor - 20)) + 500 + 600 + LevelHealth
-
-if vigor > 20 and vigor < 51:
-        print("Your character should have around " + str(midvigor) + " HP.")
 
 #Endurance
 
@@ -40,6 +28,15 @@ if endurance > 20:
     stamina = 80 + 40 + (endurance - 20)
 
 print("Your character should have " + str(stamina) + " total stamina.")
+
+if endurance <= 20:
+    LevelHealth = LevelHealth + (2 * endurance)
+
+elif endurance < 51:
+    LevelHealth = LevelHealth + 40 + endurance
+
+else:
+    LevelHealth = LevelHealth + 70 + 0
 
 
 #Vitality
@@ -59,6 +56,15 @@ carrying = float(input("How many pounds are you carrying?: "))
 CarryPercent = round((100 * carrying / CarryWeight), 1)
 
 print("Since you are carrying about " + str(carrying) + " pounds, you should have around " + str(CarryPercent) + "% carry capacity.")
+
+if vitality <= 20:
+    LevelHealth = LevelHealth + (2 * vitality)
+
+elif vitality < 51:
+    LevelHealth = LevelHealth + 40 + vitality
+
+else:
+    LevelHealth = LevelHealth + 70 + 0
 
 #Attunement
 
@@ -96,3 +102,97 @@ elif attunement < 75:
 
 else:
         print("You have the maximum attunement slots, 10")
+
+if attunement <= 20:
+    LevelHealth = LevelHealth + (2 * attunement)
+
+elif attunement < 51:
+    LevelHealth = LevelHealth + 40 + attunement
+
+else:
+    LevelHealth = LevelHealth + 70 + 0
+
+#Strength
+
+strength = int(input("Please enter your strength: "))
+
+if strength <= 20:
+    LevelHealth = LevelHealth + (2 * strength)
+
+elif strength < 51:
+    LevelHealth = LevelHealth + 40 + strength
+
+else:
+    LevelHealth = LevelHealth + 70 + 0
+
+#Dexterity
+
+dexterity = int(input("Please enter your dexterity: "))
+
+if dexterity <= 20:
+    LevelHealth = LevelHealth + (2 * dexterity)
+
+elif dexterity < 51:
+    LevelHealth = LevelHealth + 40 + dexterity
+
+else:
+    LevelHealth = LevelHealth + 70 + 0
+
+#Adaptability
+
+adaptability = int(input("Please enter your adaptability: "))
+
+if adaptability <= 20:
+    LevelHealth = LevelHealth + (2 * adaptability)
+
+elif adaptability < 51:
+    LevelHealth = LevelHealth + 40 + adaptability
+
+else:
+    LevelHealth = LevelHealth + 70 + 0
+
+#Intelligence
+
+intelligence = int(input("Please enter your intelligence: "))
+
+if intelligence <= 20:
+    LevelHealth = LevelHealth + (2 * intelligence)
+
+elif intelligence < 51:
+    LevelHealth = LevelHealth + 40 + intelligence
+
+else:
+    LevelHealth = LevelHealth + 70 + 0
+
+#Faith
+
+faith = int(input("Please enter your faith: "))
+
+if faith <= 20:
+    LevelHealth = LevelHealth + (2 * faith)
+
+elif faith < 51:
+    LevelHealth = LevelHealth + 40 + faith
+
+else:
+    LevelHealth = LevelHealth + 70 + 0
+
+#Final Health Calculations
+
+lowvigor = (30 * vigor) + 500 + LevelHealth
+
+if vigor < 20:
+    print("Your character should have around " + str(lowvigor) + " HP.")
+
+midvigor = (20 * (vigor - 20)) + 500 + 600 + LevelHealth
+
+if vigor >= 20 and vigor < 50:
+    print("Your character should have around " + str(midvigor) + " HP.")
+
+highvigor = (5 * (vigor - 50)) + 500 + 600 + 600 + LevelHealth
+
+if vigor >= 50 and vigor < 100:
+    print("Your character should have around " + str(highvigor) + " HP.")
+
+if vigor >= 100:
+    print("There must have been an error, since it appears that you have an impossible amount of vigor. Please try again.")
--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Copyright (c) 2014 Benjamin Massey
 
 A basic tool for finding out information about your character based on his/her stats.
+The health calculation is very complex in Dark Souls II, so the health that this
+program gives you is purely an estimate. Everything else should be exact. Contact
+me through github about any errors that you may come across while using it. This
+program requires further testing, and was mainly made as a project to practice
+programming in Python.
 
 Sources for Stat Calculation:
 * <http://darksouls2.wiki.fextralife.com/Stats>
